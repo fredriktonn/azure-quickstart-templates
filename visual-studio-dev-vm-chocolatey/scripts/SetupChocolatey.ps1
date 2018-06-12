575a75fab93e4d93d527b67b2986e1edafb9e085
Fredrik Tonn <fredrik.tonn@xlent.se>
2018-06-12 15:29:52 +0200

    Update SetupChocolatey.ps1

--- visual-studio-dev-vm-chocolatey/scripts/SetupChocolatey.ps1
+++ visual-studio-dev-vm-chocolatey/scripts/SetupChocolatey.ps1
@@ -1,9 +1,9 @@
 param([Parameter(Mandatory=$true)][string]$chocoPackages)
 cls
 
-#New-Item "c:\jdchoco" -type Directory -force | Out-Null
-#$LogFile = "c:\jdchoco\JDScript.log"
-#$chocoPackages | Out-File $LogFile -Append
+New-Item "c:\jdchoco" -type Directory -force | Out-Null
+$LogFile = "c:\jdchoco\JDScript.log"
+$chocoPackages | Out-File $LogFile -Append
 
 # Get username/password & machine name
 $userName = "artifactInstaller"
@@ -27,18 +27,18 @@ $secPassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
 
 # Ensure that current process can run scripts. 
-#"Enabling remoting" | Out-File $LogFile -Append
+"Enabling remoting" | Out-File $LogFile -Append
 Enable-PSRemoting -Force -SkipNetworkProfileCheck
 
-#"Changing ExecutionPolicy" | Out-File $LogFile -Append
+"Changing ExecutionPolicy" | Out-File $LogFile -Append
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
 # Install Choco
-#"Installing Chocolatey" | Out-File $LogFile -Append
+"Installing Chocolatey" | Out-File $LogFile -Append
 $sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
 Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
 
-#"Disabling UAC" | Out-File $LogFile -Append
+"Disabling UAC" | Out-File $LogFile -Append
 $sb = { Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0 }
 Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential
 
